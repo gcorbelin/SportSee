d4eda6271c03dc4e47efa3e2c9c2844c79e779f5
Guillaume Corbelin <guillaume.corbelin@coexya.eu>
2023-04-30 18:57:17 +0200

    Feat(Fonts): Better use of fonts with Next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { Roboto } from "next/font/google";
+import type { AppProps } from "next/app";
+
+const roboto = Roboto({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${roboto.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />;
+    </>
+  );
 }
